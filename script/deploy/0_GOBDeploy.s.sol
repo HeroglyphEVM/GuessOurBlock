1b043efbf410892e589a42fcdad53255e8cb036c
0xAtum <0xAtum@protonmail.com>
2024-10-24 14:52:41 -0400

    Deploy Test

--- script/deploy/0_GOBDeploy.s.sol
+++ script/deploy/0_GOBDeploy.s.sol
@@ -14,7 +14,7 @@ contract GOBDeploy is BaseScript {
     string private constant APX_ETH_CONTRACT_NAME = "ApxEth";
     string private constant MOCK_VAULT_CONTRACT_NAME = "MockVault";
     address private constant HEROGLYPH_RELAY = 0xa30cCE750cbE9664A0e46C323Fa2ed5376B25A93;
-    address private constant APX_ETH = 0x04C154b66CB340F3Ae24111CC767e0184Ed00Cc6;
+    address private constant APX_ETH = 0x9Ba021B0a9b958B5E75cE9f6dff97C7eE52cb3E6;
 
     struct ProtocolConfig {
         address owner;
@@ -27,17 +27,20 @@ contract GOBDeploy is BaseScript {
 
     ProtocolConfig private config;
 
+    address private gobSender;
+    bool private gobSenderExists;
+    address private gobReceiver;
+    bool private gobReceiverExists;
+    address private vault;
+    bool private vaultExists;
+
     function run() external override {
         config = abi.decode(vm.parseJson(_getConfig(CONFIG_FILE), string.concat(".", _getNetwork())), (ProtocolConfig));
         _loadContracts(false);
 
-        address vault;
-        address gobReceiver;
-        bool gobExists;
-
         // Arbitrum
         if (block.chainid == 42_161) {
-            _tryDeployContract(
+            (gobSender, gobSenderExists) = _tryDeployContract(
                 GOB_SENDER_CONTRACT_NAME,
                 0,
                 type(GuessOurBlockSender).creationCode,
@@ -46,42 +49,71 @@ contract GOBDeploy is BaseScript {
         }
         // Ethereum
         else if (block.chainid == 1) {
-            (vault,) = _tryDeployContract(
-                APX_ETH_CONTRACT_NAME,
-                0,
-                type(apxETHVault).creationCode,
-                abi.encode(_getDeployerAddress(), address(0), APX_ETH, config.treasury)
-            );
-
-            (gobReceiver, gobExists) = _tryDeployContract(
+            (gobReceiver, gobReceiverExists) = _tryDeployContract(
                 GOB_RECEIVER_CONTRACT_NAME,
                 0,
                 type(GuessOurBlockReceiver).creationCode,
                 abi.encode(config.receiverLzEndpoint, _getDeployerAddress(), config.treasury, address(0))
             );
+            (vault, vaultExists) = _tryDeployContract(
+                APX_ETH_CONTRACT_NAME,
+                0,
+                type(apxETHVault).creationCode,
+                abi.encode(_getDeployerAddress(), gobReceiver, APX_ETH, config.treasury)
+            );
         }
         // Sepolia
         else if (block.chainid == 11_155_111) {
-            (vault,) = _tryDeployContract(
-                MOCK_VAULT_CONTRACT_NAME,
-                0,
-                type(MockVault).creationCode,
-                abi.encode(_getDeployerAddress(), address(0), config.treasury)
-            );
-
-            (gobReceiver, gobExists) = _tryDeployContract(
+            (gobReceiver, gobReceiverExists) = _tryDeployContract(
                 GOB_RECEIVER_CONTRACT_NAME,
                 0,
                 type(GuessOurBlockReceiver).creationCode,
                 abi.encode(config.receiverLzEndpoint, _getDeployerAddress(), _getDeployerAddress())
             );
+            (vault,) = _tryDeployContract(
+                MOCK_VAULT_CONTRACT_NAME,
+                0,
+                type(MockVault).creationCode,
+                abi.encode(_getDeployerAddress(), gobReceiver, config.treasury)
+            );
         } else {
             revert("Unsupported network");
         }
 
-        if (!gobExists) {
+        if (!gobReceiverExists && block.chainid != 42_161) {
             vm.broadcast(_getDeployerPrivateKey());
             GuessOurBlockReceiver(payable(gobReceiver)).updateDripVault(vault);
+        }
+
+        test_deployment();
+    }
+
+    function test_deployment() internal view {
+        if (!_isSimulation()) return;
+
+        if (block.chainid == 42_161 && !gobSenderExists) {
+            GuessOurBlockSender gobSenderContract = GuessOurBlockSender(payable(gobSender));
+
+            assert(gobSenderContract.lzEndpointReceiverId() == config.receiverLzId);
+            assert(gobSenderContract.heroglyphRelay() == HEROGLYPH_RELAY);
+            assert(address(gobSenderContract.endpoint()) == config.senderLzEndpoint);
+            assert(gobSenderContract.owner() == _getDeployerAddress());
+        } else if (block.chainid == 1) {
+            if (!gobReceiverExists) {
+                GuessOurBlockReceiver gobReceiverContract = GuessOurBlockReceiver(payable(gobReceiver));
+
+                assert(gobReceiverContract.owner() == _getDeployerAddress());
+                assert(gobReceiverContract.treasury() == config.treasury);
+                assert(address(gobReceiverContract.endpoint()) == config.receiverLzEndpoint);
+            }
+            if (!vaultExists) {
+                apxETHVault vaultContract = apxETHVault(payable(vault));
+
+                assert(vaultContract.owner() == _getDeployerAddress());
+                assert(vaultContract.gob() == gobReceiver);
+                assert(vaultContract.rateReceiver() == config.treasury);
+                assert(address(vaultContract.apxETH()) == APX_ETH);
+            }
         }
     }
 }
